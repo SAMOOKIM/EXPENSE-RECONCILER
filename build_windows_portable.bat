1e79b8920a8622336800d5fe05d4866d4121bc3f
SAMOOKIM <kjg0705@samoocm.com>
2026-08-05 10:47:32 +0900

    Simplify build script for Windows portable version
    Removed virtual environment setup and updated build message.

--- build_windows_portable.bat
+++ build_windows_portable.bat
@@ -7,9 +7,6 @@ echo Expense Reconciler - Windows Portable Build
 echo ============================================
 echo.
 
-python -m venv .buildenv
-call .buildenv\Scripts\activate.bat
-
 python -m pip install --upgrade pip
 python -m pip install -r requirements.txt
 python -m pip install pyinstaller
@@ -19,4 +16,4 @@ echo Building portable EXE...
 python -m PyInstaller --clean --noconfirm Expense_Reconciler.spec
 
 echo.
-echo Build finished. See dist\Expense_Reconciler.exe
+echo Build finished.
